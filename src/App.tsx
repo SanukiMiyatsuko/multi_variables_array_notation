2b6b9f7f4cc1bfa2b7ff1026ed365ae1a78d2a48
taketori <naoki123453@gmail.com>
2024-08-06 01:09:40 +0900

    修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 import { headNameReplace, Scanner } from "./parse";
-import ReactMarkdown from 'react-markdown';
-import remarkMath from 'remark-math';
-import rehypeKatex from 'rehype-katex';
-import 'katex/dist/katex.min.css';
-import { Hyouki, less_than, Options, termToString, variable_length, equalize, T, loose } from './intersection';
-import { switchFunc } from './junction';
+import ReactMarkdown from "react-markdown";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+import "katex/dist/katex.min.css";
+import {
+  Hyouki,
+  less_than,
+  Options,
+  termToString,
+  variable_length,
+  equalize,
+  T,
+  loose,
+} from "./intersection";
+import { switchFunc } from "./junction";
 
 type Operation = "fund" | "dom" | "less_than";
 
@@ -55,12 +64,19 @@ function App() {
       if (operation === "less_than") {
         if (!y) throw Error("Bの入力が必要です");
         inputStry = termToString(y, options, selected, lambda);
-        inputStr = options.checkOnOffT ? `入力：$${inputStrx} \\lt ${inputStry}$` : `入力：${inputStrx} < ${inputStry}`;
+        inputStr = options.checkOnOffT
+          ? `入力：$${inputStrx} \\lt ${inputStry}$`
+          : `入力：${inputStrx} < ${inputStry}`;
         setOutput(`${inputStr}\n\n出力：${less_than(x, y) ? "真" : "偽"}`);
         return;
       }
 
-      const reversed = inputC.split('').reverse().join('');
+      if (selected === "C") {
+        if (inputC === "") throw Error("ψコードの入力が必要です");
+        if (!purseCode(inputC)) throw Error("ψコードではない文字列です");
+      }
+
+      const reversed = inputC.split("").reverse().join("");
 
       const func: Hyouki = switchFunc(selected);
       let result: T;
@@ -69,10 +85,14 @@ function App() {
           case "fund":
             if (!y) throw Error("Bの入力が必要です");
             inputStry = termToString(y, options, selected, lambda);
-            inputStr = options.checkOnOffT ? `入力：$${inputStrx}[${inputStry}]$` : `入力：${inputStrx}[${inputStry}]`;
+            inputStr = options.checkOnOffT
+              ? `入力：$${inputStrx}[${inputStry}]$`
+              : `入力：${inputStrx}[${inputStry}]`;
             return func.fund(x, y, reversed);
           case "dom":
-            inputStr = options.checkOnOffT ? `入力：$\\textrm{dom}(${inputStrx})$` : `入力：dom(${inputStrx})`;
+            inputStr = options.checkOnOffT
+              ? `入力：$\\textrm{dom}(${inputStrx})$`
+              : `入力：dom(${inputStrx})`;
             return func.dom(x, reversed);
           default:
             throw new Error("不明な操作");
@@ -101,37 +121,60 @@ function App() {
     }));
   };
 
-  const inputcode = (inputcode: string) => {
-    setInputC(inputcode);
-    setCodeComment("");
+  const purseCode = (inputcode: string): boolean => {
     let result: boolean;
     if (selected === "C") {
       result = /^([RF]*F)$/.test(inputcode);
     } else {
       result = /^([MRF]*F)$/.test(inputcode);
-      if (result) result = /^(.*[RF]F)$/.test(inputcode) || inputcode.length === 1;
+      if (result)
+        result = /^(.*[RF]F)$/.test(inputcode) || inputcode.length === 1;
     }
-    if (!result) setCodeComment("ψコードではない文字列です");
+    return result;
+  };
+
+  const inputcode = (inputcode: string) => {
+    setInputC(inputcode);
+    setCodeComment("");
+    if (!purseCode(inputcode)) setCodeComment("ψコードではない文字列です");
     return;
-  }
+  };
 
   return (
     <div className="app">
       <header>多変数配列表記計算機</header>
       <main>
         <p className="rdm">
-        入力はψ(a_n,a_&#123;n-1&#125;,...,a_3,a_2,a_1), ψ_&#123;a_n&#125;(a_&#123;n-1&#125;,...,a_3,a_2,a_1)の形式で行ってください。<br />
-          変数の個数はばらばらでも大丈夫です。<br />
-          {(selected !== "ψ" && selected !== "C") && <>ψは{selected}としても大丈夫です。<br /></>}
-          _, &#123;, &#125;は省略可能です。<br />
-          略記として、1 := ψ(0,0,...,0,0,0), n := 1 + 1 + ...(n個の1)... + 1, ω := ψ(0,0,...,0,0,1), Ω := ψ(0,0,...,0,1,0), I := ψ(0,0,...,1,0,0)が使用可能。<br />
-          また、ψは"p"で、{(selected !== "ψ" && selected !== "C") && <>または{selected}は"{headNameReplace(selected)}"で、</>}ωはwで、ΩはWで代用可能です。
+          入力はψ(a_n,a_&#123;n-1&#125;,...,a_3,a_2,a_1),
+          ψ_&#123;a_n&#125;(a_&#123;n-1&#125;,...,a_3,a_2,a_1)の形式で行ってください。
+          <br />
+          変数の個数はばらばらでも大丈夫です。
+          <br />
+          {selected !== "ψ" && selected !== "C" && (
+            <>
+              ψは{selected}としても大丈夫です。
+              <br />
+            </>
+          )}
+          _, &#123;, &#125;は省略可能です。
+          <br />
+          略記として、1 := ψ(0,0,...,0,0,0), n := 1 + 1 + ...(n個の1)... + 1, ω
+          := ψ(0,0,...,0,0,1), Ω := ψ(0,0,...,0,1,0), I :=
+          ψ(0,0,...,1,0,0)が使用可能。
+          <br />
+          また、ψは"p"で、
+          {selected !== "ψ" && selected !== "C" && (
+            <>
+              または{selected}は"{headNameReplace(selected)}"で、
+            </>
+          )}
+          ωはwで、ΩはWで代用可能です。
         </p>
         A:
         <input
           className="input is-primary"
           value={inputA}
-          onChange={e => setInputA(e.target.value)}
+          onChange={(e) => setInputA(e.target.value)}
           type="text"
           placeholder="入力A"
         />
@@ -139,21 +182,24 @@ function App() {
         <input
           className="input is-primary"
           value={inputB}
-          onChange={e => setInputB(e.target.value)}
+          onChange={(e) => setInputB(e.target.value)}
           type="text"
           placeholder="入力B"
         />
-        {selected === "C" &&
-          <>ψコード:
-          <input
-            className="input is-primary"
-            value={inputC}
-            onChange={e => inputcode(e.target.value)}
-            type="text"
-            placeholder="CODE"
-          /><br />
-          {codeComment}</>
-        }
+        {selected === "C" && (
+          <>
+            ψコード:
+            <input
+              className="input is-primary"
+              value={inputC}
+              onChange={(e) => inputcode(e.target.value)}
+              type="text"
+              placeholder="CODE"
+            />
+            <br />
+            {codeComment}
+          </>
+        )}
         <div className="block">
           <button className="button is-primary" onClick={() => compute("fund")}>
             A[B]を計算
@@ -161,11 +207,17 @@ function App() {
           <button className="button is-primary" onClick={() => compute("dom")}>
             dom(A)を計算
           </button>
-          <button className="button is-primary" onClick={() => compute("less_than")}>
+          <button
+            className="button is-primary"
+            onClick={() => compute("less_than")}
+          >
             A &lt; Bか判定
           </button>
           <div className="select is-rounded">
-            <select value={selected} onChange={e => setSelected(e.target.value)}>
+            <select
+              value={selected}
+              onChange={(e) => setSelected(e.target.value)}
+            >
               <option value="〇">多変数〇関数</option>
               <option value="亜">多変数亜関数</option>
               <option value="亞">多変数亞関数</option>
@@ -174,45 +226,108 @@ function App() {
             </select>
           </div>
         </div>
-        <input type="button" value="オプション" onClick={() => setShowHide(!showHide)} className="button is-primary is-light is-small" />
+        <input
+          type="button"
+          value="オプション"
+          onClick={() => setShowHide(!showHide)}
+          className="button is-primary is-light is-small"
+        />
         {showHide && (
           <ul>
-            <li><label className="checkbox">
-              <input type="checkbox" checked={options.checkOnOffo} onChange={() => handleCheckboxChange('checkOnOffo')} />
-              &nbsp;{(options.checkOnOffp || selected === "C") ? "ψ" : selected}(0,0,...,0,0,1)をωで出力
-            </label></li>
-            <li><label className="checkbox">
-              <input type="checkbox" checked={options.checkOnOffO} onChange={() => handleCheckboxChange('checkOnOffO')} />
-              &nbsp;{(options.checkOnOffp || selected === "C") ? "ψ" : selected}(0,0,...,0,1,0)をΩで出力
-            </label></li>
-            <li><label className="checkbox">
-              <input type="checkbox" checked={options.checkOnOffI} onChange={() => handleCheckboxChange('checkOnOffI')} />
-              &nbsp;{(options.checkOnOffp || selected === "C") ? "ψ" : selected}(0,0,...,1,0,0)をIで出力
-            </label></li>
-            <li><label className="checkbox">
-            <input type="checkbox" checked={options.checkOnOffF} onChange={() => handleCheckboxChange('checkOnOffF')} />
-              変数の個数を最大数で固定して表示
-            </label></li>
-            <li><label className="checkbox">
-              <input type="checkbox" checked={options.checkOnOffA} onChange={() => handleCheckboxChange('checkOnOffA')} />
-              &nbsp;{(options.checkOnOffp || selected === "C") ? "ψ" : selected}(a_n,a_&#123;n-1&#125;,...,a_3,a_2,a_1)を{(options.checkOnOffp || selected === "C") ? "ψ" : selected}_&#123;a_n&#125;(a_n,a_&#123;n-1&#125;,...,a_3,a_2,a_1)で表示
-            </label></li>
+            <li>
+              <label className="checkbox">
+                <input
+                  type="checkbox"
+                  checked={options.checkOnOffo}
+                  onChange={() => handleCheckboxChange("checkOnOffo")}
+                />
+                &nbsp;{options.checkOnOffp || selected === "C" ? "ψ" : selected}
+                (0,0,...,0,0,1)をωで出力
+              </label>
+            </li>
+            <li>
+              <label className="checkbox">
+                <input
+                  type="checkbox"
+                  checked={options.checkOnOffO}
+                  onChange={() => handleCheckboxChange("checkOnOffO")}
+                />
+                &nbsp;{options.checkOnOffp || selected === "C" ? "ψ" : selected}
+                (0,0,...,0,1,0)をΩで出力
+              </label>
+            </li>
+            <li>
+              <label className="checkbox">
+                <input
+                  type="checkbox"
+                  checked={options.checkOnOffI}
+                  onChange={() => handleCheckboxChange("checkOnOffI")}
+                />
+                &nbsp;{options.checkOnOffp || selected === "C" ? "ψ" : selected}
+                (0,0,...,1,0,0)をIで出力
+              </label>
+            </li>
+            <li>
+              <label className="checkbox">
+                <input
+                  type="checkbox"
+                  checked={options.checkOnOffF}
+                  onChange={() => handleCheckboxChange("checkOnOffF")}
+                />
+                変数の個数を最大数で固定して表示
+              </label>
+            </li>
+            <li>
+              <label className="checkbox">
+                <input
+                  type="checkbox"
+                  checked={options.checkOnOffA}
+                  onChange={() => handleCheckboxChange("checkOnOffA")}
+                />
+                &nbsp;{options.checkOnOffp || selected === "C" ? "ψ" : selected}
+                (a_n,a_&#123;n-1&#125;,...,a_3,a_2,a_1)を
+                {options.checkOnOffp || selected === "C" ? "ψ" : selected}
+                _&#123;a_n&#125;(a_n,a_&#123;n-1&#125;,...,a_3,a_2,a_1)で表示
+              </label>
+            </li>
             {options.checkOnOffA && (
-              <li><ul><li><label className="checkbox">
-                <input type="checkbox" checked={options.checkOnOffB} onChange={() => handleCheckboxChange('checkOnOffB')} />
-                &nbsp;全ての&#123; &#125;を表示
-              </label></li></ul></li>
+              <li>
+                <ul>
+                  <li>
+                    <label className="checkbox">
+                      <input
+                        type="checkbox"
+                        checked={options.checkOnOffB}
+                        onChange={() => handleCheckboxChange("checkOnOffB")}
+                      />
+                      &nbsp;全ての&#123; &#125;を表示
+                    </label>
+                  </li>
+                </ul>
+              </li>
             )}
-            {(selected !== "ψ" && selected !== "C") && (
-              <li><label className="checkbox">
-                <input type="checkbox" checked={options.checkOnOffp} onChange={() => handleCheckboxChange('checkOnOffp')} />
-                &nbsp;{selected}をψで表示
-              </label></li>
+            {selected !== "ψ" && selected !== "C" && (
+              <li>
+                <label className="checkbox">
+                  <input
+                    type="checkbox"
+                    checked={options.checkOnOffp}
+                    onChange={() => handleCheckboxChange("checkOnOffp")}
+                  />
+                  &nbsp;{selected}をψで表示
+                </label>
+              </li>
             )}
-            <li><label className="checkbox">
-              <input type="checkbox" checked={options.checkOnOffT} onChange={() => handleCheckboxChange('checkOnOffT')} />
-              &nbsp;TeXで出力
-            </label></li>
+            <li>
+              <label className="checkbox">
+                <input
+                  type="checkbox"
+                  checked={options.checkOnOffT}
+                  onChange={() => handleCheckboxChange("checkOnOffT")}
+                />
+                &nbsp;TeXで出力
+              </label>
+            </li>
           </ul>
         )}
         <div className="box is-primary">
@@ -231,11 +346,87 @@ function App() {
         </div>
       </main>
       <footer>
-        <a href="https://googology.fandom.com/ja/wiki/%E3%83%A6%E3%83%BC%E3%82%B6%E3%83%BC%E3%83%96%E3%83%AD%E3%82%B0:Naruyoko/%EF%BC%9F%E2%86%92%CF%86%E2%86%92%CF%88%E2%86%92%E4%B8%89#%E5%A4%9A%E5%A4%89%E6%95%B0%E3%80%87%E9%96%A2%E6%95%B0" target="_blank" rel="noreferrer">Definition of "Multi Variables 〇 Function"</a> by <a href="https://googology.fandom.com/ja/wiki/%E3%83%A6%E3%83%BC%E3%82%B6%E3%83%BC:Naruyoko" target="_blank" rel="noreferrer">Naruyoko</a>, Retrieved 2024/08/03 <br />
-        <a href="https://googology.fandom.com/ja/wiki/%E3%83%A6%E3%83%BC%E3%82%B6%E3%83%BC%E3%83%96%E3%83%AD%E3%82%B0:Mitsuki1729/%E8%A9%A6%E4%BD%9C:%E3%81%8F%E3%81%BE%E3%81%8F%E3%81%BE(%E5%A4%A7%E5%98%98)%E5%A4%9A%E5%A4%89%E6%95%B0%CE%A8" target="_blank" rel="noreferrer">Definition of "Kumakuma (Ohuso) Multi Variables ψ Function"</a> by <a href="https://googology.fandom.com/ja/wiki/%E3%83%A6%E3%83%BC%E3%82%B6%E3%83%BC:Mitsuki1729" target="_blank" rel="noreferrer">Mitsuki1729</a>, Retrieved 2024/08/03 <br />
-        <a href="https://googology.fandom.com/ja/wiki/%E3%83%A6%E3%83%BC%E3%82%B6%E3%83%BC%E3%83%96%E3%83%AD%E3%82%B0:%E3%81%BF%E3%81%9A%E3%81%A9%E3%82%89/2%E7%A8%AE%E9%A1%9E%E3%81%AE%E9%96%89%E7%82%B9%E3%81%AB%E3%82%88%E3%82%8B%E5%A4%9A%E5%A4%89%E6%95%B0%E9%85%8D%E5%88%97%E3%81%AE%E5%88%86%E9%A1%9E" target="_blank" rel="noreferrer">Definition of "ψ code"</a> by <a href="https://googology.fandom.com/ja/wiki/%E3%83%A6%E3%83%BC%E3%82%B6%E3%83%BC:%E3%81%BF%E3%81%9A%E3%81%A9%E3%82%89" target="_blank" rel="noreferrer">みずどら</a>, Retrieved 2024/08/06 <br />
-        <a href="https://googology.fandom.com/ja/wiki/%E3%83%A6%E3%83%BC%E3%82%B6%E3%83%BC%E3%83%96%E3%83%AD%E3%82%B0:%E7%AB%B9%E5%8F%96%E7%BF%81/%E3%83%96%E3%83%AD%E3%82%B0%E8%A8%98%E4%BA%8B%E3%81%BE%E3%81%A8%E3%82%81" target="_blank" rel="noreferrer">Definition of other functions</a> by <a href="https://googology.fandom.com/ja/wiki/%E3%83%A6%E3%83%BC%E3%82%B6%E3%83%BC:%E7%AB%B9%E5%8F%96%E7%BF%81" target="_blank" rel="noreferrer">竹取翁</a>, Retrieved 2024/08/03 <br />
-        The program <a href="https://github.com/SanukiMiyatsuko/multi_variables_array_notation" target="_blank" rel="noreferrer">https://github.com/SanukiMiyatsuko/multi_variables_array_notation</a> is licensed by <a href="https://creativecommons.org/licenses/by-sa/3.0/legalcode" target="_blank" rel="noreferrer">Creative Commons Attribution-ShareAlike 3.0 Unported License</a>.<br />
+        <a
+          href="https://googology.fandom.com/ja/wiki/%E3%83%A6%E3%83%BC%E3%82%B6%E3%83%BC%E3%83%96%E3%83%AD%E3%82%B0:Naruyoko/%EF%BC%9F%E2%86%92%CF%86%E2%86%92%CF%88%E2%86%92%E4%B8%89#%E5%A4%9A%E5%A4%89%E6%95%B0%E3%80%87%E9%96%A2%E6%95%B0"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Definition of "Multi Variables 〇 Function"
+        </a>{" "}
+        by{" "}
+        <a
+          href="https://googology.fandom.com/ja/wiki/%E3%83%A6%E3%83%BC%E3%82%B6%E3%83%BC:Naruyoko"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Naruyoko
+        </a>
+        , Retrieved 2024/08/03 <br />
+        <a
+          href="https://googology.fandom.com/ja/wiki/%E3%83%A6%E3%83%BC%E3%82%B6%E3%83%BC%E3%83%96%E3%83%AD%E3%82%B0:Mitsuki1729/%E8%A9%A6%E4%BD%9C:%E3%81%8F%E3%81%BE%E3%81%8F%E3%81%BE(%E5%A4%A7%E5%98%98)%E5%A4%9A%E5%A4%89%E6%95%B0%CE%A8"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Definition of "Kumakuma (Ohuso) Multi Variables ψ Function"
+        </a>{" "}
+        by{" "}
+        <a
+          href="https://googology.fandom.com/ja/wiki/%E3%83%A6%E3%83%BC%E3%82%B6%E3%83%BC:Mitsuki1729"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Mitsuki1729
+        </a>
+        , Retrieved 2024/08/03 <br />
+        <a
+          href="https://googology.fandom.com/ja/wiki/%E3%83%A6%E3%83%BC%E3%82%B6%E3%83%BC%E3%83%96%E3%83%AD%E3%82%B0:%E3%81%BF%E3%81%9A%E3%81%A9%E3%82%89/2%E7%A8%AE%E9%A1%9E%E3%81%AE%E9%96%89%E7%82%B9%E3%81%AB%E3%82%88%E3%82%8B%E5%A4%9A%E5%A4%89%E6%95%B0%E9%85%8D%E5%88%97%E3%81%AE%E5%88%86%E9%A1%9E"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Definition of "ψ code"
+        </a>{" "}
+        by{" "}
+        <a
+          href="https://googology.fandom.com/ja/wiki/%E3%83%A6%E3%83%BC%E3%82%B6%E3%83%BC:%E3%81%BF%E3%81%9A%E3%81%A9%E3%82%89"
+          target="_blank"
+          rel="noreferrer"
+        >
+          みずどら
+        </a>
+        , Retrieved 2024/08/06 <br />
+        <a
+          href="https://googology.fandom.com/ja/wiki/%E3%83%A6%E3%83%BC%E3%82%B6%E3%83%BC%E3%83%96%E3%83%AD%E3%82%B0:%E7%AB%B9%E5%8F%96%E7%BF%81/%E3%83%96%E3%83%AD%E3%82%B0%E8%A8%98%E4%BA%8B%E3%81%BE%E3%81%A8%E3%82%81"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Definition of other functions
+        </a>{" "}
+        by{" "}
+        <a
+          href="https://googology.fandom.com/ja/wiki/%E3%83%A6%E3%83%BC%E3%82%B6%E3%83%BC:%E7%AB%B9%E5%8F%96%E7%BF%81"
+          target="_blank"
+          rel="noreferrer"
+        >
+          竹取翁
+        </a>
+        , Retrieved 2024/08/03 <br />
+        The program{" "}
+        <a
+          href="https://github.com/SanukiMiyatsuko/multi_variables_array_notation"
+          target="_blank"
+          rel="noreferrer"
+        >
+          https://github.com/SanukiMiyatsuko/multi_variables_array_notation
+        </a>{" "}
+        is licensed by{" "}
+        <a
+          href="https://creativecommons.org/licenses/by-sa/3.0/legalcode"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Creative Commons Attribution-ShareAlike 3.0 Unported License
+        </a>
+        .<br />
         Last updated: 2024/08/06
       </footer>
     </div>
